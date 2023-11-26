8c91797c5e5406aa62fd113dda0b02a391e0f530
Petr Gregor <petr.gregor@email.cz>
2023-11-26 09:53:29 +0100

    Decorators.

--- 20_decorators.py
+++ 20_decorators.py
@@ -1,5 +1,50 @@
 # dekorátory
+from datetime import datetime
 
-#Petr Symerský provedl změnu
 
-# pokus o změnu
+def vypni_v_noci(funkce):
+    # Dekorátor, který volá dekorovanou funkci pouze během dne.
+    def wrapper():
+        if 7 <= datetime.now().hour < 22:
+            funkce()
+
+    return wrapper
+
+
+@vypni_v_noci
+def pozdrav():
+    print("Ahoj Světe")
+
+
+def pozdrav_bez_dekoratoru():
+    if 7 <= datetime.now().hour < 22:
+        print("Ahoj Světe")
+
+pozdrav_bez_dekoratoru()
+pozdrav()
+#vypni_v_noci(pozdrav())
+
+def pust_v_rozpeti(od=7, do=22, function_name=""):
+    # Dekorátor, který volá dekorovanou funkci pouze v zadaném časovém rozpětí
+    def dekorator(dekorovana_funkce):
+        def wrapper():
+            if od <= datetime.now().hour < do:
+                print(f"Výpis z funkce {function_name} {datetime.now()}: ", end="")
+                dekorovana_funkce()
+
+        return wrapper
+
+    return dekorator
+
+
+@pust_v_rozpeti(9, 14, "pozdrav2")
+def pozdrav2():
+    print("Ahoj Světe")
+
+
+@pust_v_rozpeti(9, 20, "pozdrav3")
+def pozdrav3():
+    print("Ahoj Světe")
+
+pozdrav2()
+pozdrav3()

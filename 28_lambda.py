76fb340d4e9d75e118156a0ad670a4b154865882
Petr Gregor <petr.gregor@email.cz>
2023-12-02 10:24:17 +0100

    Lambda functions: map, filter, reduce.

--- 28_lambda.py
+++ 28_lambda.py
@@ -25,3 +25,46 @@ def equals(x, y):
 lambda_equals = lambda x, y: x == y
 print(lambda_equals(5, 5.0))
 
+# map funkce: map(funkce, seznam argumentů)
+# příklad: chceme všechny hodnoty seznamu dát na druhou
+## pomocí for cyklu
+my_list = [1, 2, 3, 4, 5]
+results = []
+for n in my_list:
+    results.append(square(n))
+print(f"results:    {results}")
+
+## pomocí funkce map a definované funkce square
+def square(x):
+    return x ** 2
+print(f"pomocí map: {list(map(square, my_list))}")
+## to stejné pomocí labda výrazu
+print(f"lambda:     {list(map(lambda x: x ** 2, my_list))}")
+
+# funkce filter
+## příklad: chceme vybrat pouze liché hodnoty
+print("Funkce filter:")
+my_list = [1, 2, 3, 4, 5]
+results = []
+for n in my_list:
+    if n % 2 == 1:  # liché číslo (nepárné)
+        results.append(n)
+print(f"results:    {results}")
+
+# to stejné pomofí filter
+print(f"liché?:     {list(map(lambda x: x % 2 == 1, my_list))}")
+print(f"lambda:     {list(filter(lambda x: x % 2, my_list))}")
+
+# funkce reduce
+from functools import reduce
+# příklad: chceme sečíst všechny hodnoty v seznamu
+print("Funkce reduce:")
+my_list = [1, 2, 3, 4, 5]
+result = 0
+for n in my_list:
+    result += n
+print(f"Součet je:  {result}")
+
+# pomocí funkce reduce
+print(f"reduce:     {reduce(lambda x, y: x + y, my_list)}")
+

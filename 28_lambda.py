fd4c21571a03e34e7f5ff4cf725b8b3ac8ab94fb
Petr Gregor <petr.gregor@email.cz>
2023-12-02 11:33:10 +0100

    Lambda functions: úkoly.

--- 28_lambda.py
+++ 28_lambda.py
@@ -1,4 +1,7 @@
 # funkce vezme text a vrátí ho malými písmeny
+from dataclasses import dataclass
+
+
 def to_lower(s):
     return s.lower()
 
@@ -79,3 +82,40 @@ print(f"max:        {max(pars, key=lambda x: x[1])}")
 
 print(f"max:        {max(pars, key=lambda x: x[0] + x[1])}")
 
+# definujte třídu Person, která bude mít atributy first_name a last_name
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+
+# vytvořte několik instancí třídy Person
+# vložte je do seznamu
+persons = [Person("Martin", "Novák"),
+           Person("Adéla", "Svobodová"),
+           Person("Radim", "Brzobohatý")]
+
+print(persons)
+
+# úkol: uspořádat abecedně dle příjmení (last_name)
+print(f"sorted by last_name:  {sorted(persons, key=lambda x: x.last_name)}")
+
+# úkol: uspořádat abecedně dle jména (first_name)
+print(f"sorted by first_name: {sorted(persons, key=lambda x: x.first_name)}")
+
+# do třídy Person přidejte atribut salary (příjem)
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    salary: int
+
+    def __repr__(self):
+        return f"({self.first_name}, {self.last_name}, {self.salary})"
+
+# vytvořte seznam
+persons = [Person("Martin", "Novák", 25000),
+           Person("Adéla", "Svobodová", 35000),
+           Person("Radim", "Brzobohatý", 32500)]
+
+# uspořádejte podle příjmu sestupně
+print(f"sorted by salary:     {sorted(persons, key=lambda x: x.salary, reverse=True)}")  # sestupně
